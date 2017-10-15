f4f254e7347b91f15a31428445b5eed383c86ed9
Admir Tiric <Admirtiric25@Hotmail.com>
2017-10-14 17:09:06 -0700

    Website

---
+++ js/second.js
@@ -0,0 +1,18 @@
+
+$("#winestock").click(function() {
+    $('html, body').animate({
+        scrollTop: parseInt($("#gallery").offset().top)
+    }, 1000);
+});
+
+$("#aboutone").click(function() {
+    $('html, body').animate({
+        scrollTop: parseInt($("#about-header").offset().top)
+    }, 1000);
+});
+
+$("#home").click(function() {
+    $('html, body').animate({
+        scrollTop: parseInt($("#myCarousel").offset().top)
+    }, 1000);
+});
